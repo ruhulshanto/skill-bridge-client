4544f9d0a9e56ea83699f02f154757320a6ce0a3
ruhulshanto <ruhulshanto8082@gmail.com>
2026-02-03 03:04:50 +0600

    Fix: Removed non-existent HowItWorks component import from Home page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,13 +1,11 @@
 import CategoriesSection from "@/components/home/categories-section";
 import FeaturedTutors from "@/components/home/featured-tutors";
 import HeroSection from "@/components/home/hero-section";
-import HowItWorks from "@/components/home/how-it-works";
 
 export default function HomePage() {
     return (
         <>
             <HeroSection />
-            <HowItWorks />
             <CategoriesSection />
             <FeaturedTutors />
         </>
